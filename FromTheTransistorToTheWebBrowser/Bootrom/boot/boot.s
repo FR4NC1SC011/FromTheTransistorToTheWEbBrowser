3adb4ad956206e74ccb374bc795f7b440e207162
FR4NC1SC011 <fcootzg@gmail.com>
2021-02-10 18:39:25 -0700

    boot finished

--- FromTheTransistorToTheWebBrowser/Bootrom/boot/boot.s
+++ FromTheTransistorToTheWebBrowser/Bootrom/boot/boot.s
@@ -2,7 +2,22 @@
 .global init
 
 init:
-  jmp init # jump to "init"
+	mov $msg, %si		# loads the address of msg into si
+	mov $0xe, %ah		# loads 0xe (func. number for int 0x10) into ah
+print_char:
+	lodsb						# loads the byte from the add. in si into al and increments si
+	cmp $0, %al			# compares content in AL with zero
+	je done					# if al == 0 go to done
+	int $0x10				# prints the character in al to screen
+	jmp print_char  # repeat with next byte
+done:
+	hlt							# stop execution
 
+
+
+
+msg: .asciz "Hello world!"
 
 .fill 510-(. - init), 1, 0 # add zeroes to make it 510 bytes long
+
+.word 0xaa5 							# bytes that tell BIOS that this is bootable

cb432b165c21b4812fa4c71177e6c994792b20cd
FR4NC1SC011 <fcootzg@gmail.com>
2021-02-12 16:13:38 -0700

    Verilog tutorial

---
+++ FromTheTransistorToTheWebBrowser/Verilog/eg1.v
@@ -0,0 +1,17 @@
+module eg1(a,b,sel,clk,en,f);
+//Inputs - Outputs
+input a,b,sel,clk,en;
+output f;
+wire f;
+
+// Descripcion de los nodos internos
+reg net4,net5;
+wire net1,net2,net3;
+
+// Descripcion del dis.
+
+
+
+
+
+endmodule

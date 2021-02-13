57477afefa6732fa1d8a7ba72049ea11b9e7e537
FR4NC1SC011 <fcootzg@gmail.com>
2021-02-12 20:24:35 -0700

    Learning haskell

---
+++ FromTheTransistorToTheWebBrowser/haskell/first.hs
@@ -0,0 +1,12 @@
+doubleMe x = x + x
+
+doubleUs x y = x*2 + y*2
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x * 2
+
+-- otra forma de escribir la misma funcion
+-- doubleSmallNumber x = (if x > then x else x*2) + 1
+
+conanO'Brien = "It's a-me, Conan O'Brien!"

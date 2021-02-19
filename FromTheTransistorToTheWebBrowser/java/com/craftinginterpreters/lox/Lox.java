31097f40e0eaa077fbb45a0b2c11d58d52d836e9
FR4NC1SC011 <fcootzg@gmail.com>
2021-02-19 16:53:55 -0700

    Writing up the parser

--- FromTheTransistorToTheWebBrowser/java/com/craftinginterpreters/lox/Lox.java
+++ FromTheTransistorToTheWebBrowser/java/com/craftinginterpreters/lox/Lox.java
@@ -54,11 +54,13 @@ public class Lox {
   private static void run (String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
 
-    // For now just print tokens
-    for (Token token : tokens) {
-      System.out.println(token);
-    }
+    if(hadError) return;
+
+    system.out.println(new AstPrinter().print(expression));
+
   }
 
 
@@ -70,5 +72,14 @@ public class Lox {
   private static void report (int line, String where, String message) {
     System.err.println("[line " + line + "] Error" + where + ": " + message);
     hadError = true;
+  }
+  
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at'" + token.lexeme + "'", message);
+    }
   }
 }
--- FromTheTransistorToTheWebBrowser/java/com/craftinginterpreters/lox/Parser.java
+++ FromTheTransistorToTheWebBrowser/java/com/craftinginterpreters/lox/Parser.java
@@ -5,12 +5,23 @@ import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Parser {
+  private static class ParseError extends RuntimeException {}
+
+
   private final List<Token> tokens;
   private int current = 0;
 
 
   Parser(List<Token> tokens) {
     this.tokens = tokens;
+  }
+
+  Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError error) {
+      return null;
+    }
   }
 
   private Expr expression() {
@@ -95,6 +106,9 @@ class Parser {
       consume(RIGHT_PAREN, "Expect ')' after expression.");
       return new Expr.Grouping(expr);
     }
+
+
+    throw error(peek(), "Expect expression.");
   }
 
 
@@ -107,6 +121,12 @@ class Parser {
     }
 
     return false;
+  }
+
+  private Token consume(TokenType type, String message) {
+    if (check(type)) return advance();
+
+    throw error(peek(), message);
   }
 
 
@@ -130,5 +150,34 @@ class Parser {
 
   private Token previous() {
     return tokens.get(current - 1);
+  }
+
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
+  }
+
+
+  private void synchronize() {
+    advance();
+
+    while (!isAtEnd()) {
+      if (previous().type == SEMICOLON) return;
+
+      switch (peek().type) {
+        case CLASS:
+        case FUN:
+        case VAR:
+        case FOR:
+        case IF:
+        case WHILE:
+        case PRINT:
+        case RETURN:
+          return;
+      }
+
+
+      advance();
+    }
   }
 }

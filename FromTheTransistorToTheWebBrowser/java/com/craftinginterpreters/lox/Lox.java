b579118edc5a38bc45f47d8da881255f49cbb778
FR4NC1SC011 <fcootzg@gmail.com>
2021-02-20 18:05:24 -0700

    Running the interpreter

--- FromTheTransistorToTheWebBrowser/java/com/craftinginterpreters/lox/Lox.java
+++ FromTheTransistorToTheWebBrowser/java/com/craftinginterpreters/lox/Lox.java
@@ -13,7 +13,9 @@ import java.util.List;
 
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
 
 
   public static void main (String[] args) throws IOException {
@@ -34,6 +36,7 @@ public class Lox {
     run(new String(bytes, Charset.defaultCharset()));
 
     if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
   }
 
   private static void runPrompt() throws IOException {
@@ -59,7 +62,7 @@ public class Lox {
 
     if(hadError) return;
 
-    System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(expression);
 
   }
 
@@ -81,5 +84,11 @@ public class Lox {
     } else {
       report(token.line, " at'" + token.lexeme + "'", message);
     }
+  }
+
+
+  static void runtimeError (RuntimeError error) {
+    System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 }

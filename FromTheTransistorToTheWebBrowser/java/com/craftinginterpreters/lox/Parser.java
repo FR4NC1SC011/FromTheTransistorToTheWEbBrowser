0d687cc043248342ef66d2fceba5b9321fc9361f
FR4NC1SC011 <fcootzg@gmail.com>
2021-02-20 20:33:22 -0700

    Environments

--- FromTheTransistorToTheWebBrowser/java/com/craftinginterpreters/lox/Parser.java
+++ FromTheTransistorToTheWebBrowser/java/com/craftinginterpreters/lox/Parser.java
@@ -21,7 +21,7 @@ class Parser {
   List<Stmt> parse() {
     List<Stmt> statements = new ArrayList<>();
     while (!isAtEnd()) {
-      statements.add(statement());
+      statements.add(declaration());
     }
 
     return statements;
@@ -29,6 +29,18 @@ class Parser {
 
   private Expr expression() {
     return equality();
+  }
+
+
+  private Stmt declaration() {
+    try {
+      if (match(VAR)) return varDeclaration();
+      
+      return statement();
+    } catch (ParseError error) {
+      synchronize();
+      return null;
+    }
   }
 
   private Stmt statement() {
@@ -42,6 +54,19 @@ class Parser {
     Expr value = expression();
     consume(SEMICOLON, "Expect ';' after value.");
     return new Stmt.Print(value);
+  }
+
+
+  private Stmt varDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect variable name.");
+
+    Expr initializer = null;
+    if (match(EQUAL)) {
+      initializer = expression();
+    }
+
+    consume(SEMICOLON, "Expect ';' after variable declaration.");
+    return new Stmt.Var(name, initializer);
   }
 
    private Stmt expressionStatement() {
@@ -121,6 +146,10 @@ class Parser {
 
     if (match(NUMBER, STRING)) {
       return new Expr.Literal(previous().literal);
+    }
+
+    if (match(IDENTIFIER)) {
+      return new Expr.Variable(previous());
     }
 
     if (match(LEFT_PAREN)) {
--- FromTheTransistorToTheWebBrowser/java/com/craftinginterpreters/tool/GenerateAst.java
+++ FromTheTransistorToTheWebBrowser/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -18,7 +18,8 @@ public class GenerateAst {
           "Binary : Expr left, Token operator, Expr right",
           "Grouping : Expr expression",
           "Literal : Object value", 
-          "Unary : Token operator, Expr right"
+          "Unary : Token operator, Expr right",
+          "Variable : Token name"
           ));
 
     defineAst (outputDir, "Stmt", Arrays.asList(

05fe2ad808ec24df28d5b160cbea38ca5caf1e39
FR4NC1SC011 <fcootzg@gmail.com>
2021-03-19 14:09:54 -0700

    No harness tests

--- FromTheTransistorToTheWebBrowser/OS/simple_os/tests/should_panic.rs
+++ FromTheTransistorToTheWebBrowser/OS/simple_os/tests/should_panic.rs
@@ -1,26 +1,20 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
+use simple_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
 use core::panic::PanicInfo;
-use simple_os::{QemuExitCode, exit_qemu, serial_println};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
+fn should_fail() {
+    serial_println!("Should_panic::Should_fail...\t");
+    assert_eq!(1,1);
 }
 
 #[panic_handler]
@@ -30,8 +24,4 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[test_case]
-fn should_fail() {
-    serial_println!("Should_panic::Should_fail...\t");
-    assert_eq!(0,1);
-}
+

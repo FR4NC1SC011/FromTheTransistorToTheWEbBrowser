16832d68a0c27f1ebc8fdd808971bad18da04d9a
FR4NC1SC011 <fcootzg@gmail.com>
2021-03-30 13:36:05 -0700

    Interpreting the Scancodes

---
+++ FromTheTransistorToTheWebBrowser/OS/simple_os/target/x86_64-simple_os/debug/deps/simple_os-08835041676540f6.d
@@ -0,0 +1,9 @@
+/home/fco/Desktop/Projects/FromTheTransistorToTheWebBrowser/OS/simple_os/target/x86_64-simple_os/debug/deps/simple_os-08835041676540f6.rmeta: src/lib.rs src/serial.rs src/vga_buffer.rs src/interrupts.rs src/gdt.rs
+
+/home/fco/Desktop/Projects/FromTheTransistorToTheWebBrowser/OS/simple_os/target/x86_64-simple_os/debug/deps/simple_os-08835041676540f6.d: src/lib.rs src/serial.rs src/vga_buffer.rs src/interrupts.rs src/gdt.rs
+
+src/lib.rs:
+src/serial.rs:
+src/vga_buffer.rs:
+src/interrupts.rs:
+src/gdt.rs:
---
+++ FromTheTransistorToTheWebBrowser/OS/simple_os/target/x86_64-simple_os/debug/deps/simple_os-b8b4286e3a8c7f60.d
@@ -0,0 +1,5 @@
+/home/fco/Desktop/Projects/FromTheTransistorToTheWebBrowser/OS/simple_os/target/x86_64-simple_os/debug/deps/simple_os-b8b4286e3a8c7f60: src/main.rs
+
+/home/fco/Desktop/Projects/FromTheTransistorToTheWebBrowser/OS/simple_os/target/x86_64-simple_os/debug/deps/simple_os-b8b4286e3a8c7f60.d: src/main.rs
+
+src/main.rs:

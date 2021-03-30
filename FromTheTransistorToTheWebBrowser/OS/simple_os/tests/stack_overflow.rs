605bc75c754bde001a3b86066099fea9d82599c4
FR4NC1SC011 <fcootzg@gmail.com>
2021-03-29 21:50:18 -0700

    Stack Overflow Test Added

--- FromTheTransistorToTheWebBrowser/OS/simple_os/tests/stack_overflow.rs
+++ FromTheTransistorToTheWebBrowser/OS/simple_os/tests/stack_overflow.rs
@@ -1,8 +1,14 @@
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 use simple_os::serial_print;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::InterruptDescriptorTable;
+use simple_os::{exit_qemu, QemuExitCode, serial_println};
+use x86_64::structures::idt::InterruptStackFrame;
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -17,6 +23,33 @@ pub extern "C" fn _start() -> ! {
 
 }
 
+lazy_static! {
+    static ref TEST_IDT: InterruptDescriptorTable = { 
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(test_double_fault_handler)
+                .set_stack_index(simple_os::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        idt
+    };
+}
+
+
+pub fn init_test_idt() {
+    TEST_IDT.load();
+}
+
+
+extern "x86-interrupt" fn test_double_fault_handler(
+    _stack_frame: &mut InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+
 #[allow(unconditional_recursion)]
 fn stack_overflow() {
     stack_overflow(); // for each recursion, the return address is pushed
@@ -27,5 +60,5 @@ fn stack_overflow() {
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    blog_os::test_panic_handler(info)
+    simple_os::test_panic_handler(info)
 }

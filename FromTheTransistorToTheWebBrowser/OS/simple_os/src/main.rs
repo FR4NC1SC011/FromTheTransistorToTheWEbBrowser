9e48fc861b89061083cb978a41dab82d00071008
FR4NC1SC011 <fcootzg@gmail.com>
2021-03-31 16:33:06 -0700

    New mapping created

--- FromTheTransistorToTheWebBrowser/OS/simple_os/src/main.rs
+++ FromTheTransistorToTheWebBrowser/OS/simple_os/src/main.rs
@@ -12,32 +12,22 @@ entry_point!(kernel_main);
 
 fn kernel_main (boot_info: &'static BootInfo) -> ! {
     use simple_os::memory;
-    use x86_64::{structures::paging::Translate, VirtAddr};
+    use x86_64::{structures::paging::Page, VirtAddr};
+    //use x86_64::{structures::paging::Translate};
  
 
     println!("Hello world{}", "!");
     simple_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    
-    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
--- FromTheTransistorToTheWebBrowser/OS/simple_os/src/memory.rs
+++ FromTheTransistorToTheWebBrowser/OS/simple_os/src/memory.rs
@@ -1,9 +1,20 @@
 use x86_64::structures::paging::OffsetPageTable;
 
 use x86_64::{
-    structures::paging::PageTable,
-    VirtAddr,
+    PhysAddr, VirtAddr,
+    structures::paging::{
+        FrameAllocator, Mapper, Page, PageTable, PhysFrame, Size4KiB,
+    }
 };
+
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator <Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
 
 
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
@@ -23,4 +34,22 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
         let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
         &mut *page_table_ptr
+}
+
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        // FIXME: this is not safe, we do it only for testing
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
+
 }

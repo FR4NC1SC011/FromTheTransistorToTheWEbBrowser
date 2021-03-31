ec6889ec619a8fd3d1b46e921115f1ca574fe945
FR4NC1SC011 <fcootzg@gmail.com>
2021-03-30 17:09:21 -0700

    Page Faults

--- FromTheTransistorToTheWebBrowser/OS/simple_os/src/interrupts.rs
+++ FromTheTransistorToTheWebBrowser/OS/simple_os/src/interrupts.rs
@@ -30,6 +30,8 @@ lazy_static! {
         idt[InterruptIndex::Keyboard.as_usize()]
             .set_handler_fn(keyboard_interrupt_handler);
 
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
         idt
     };
 }
@@ -102,6 +104,23 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut Interrup
          PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
+}
+
+
+use x86_64::structures::idt::PageFaultErrorCode;
+use crate::hlt_loop;
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 

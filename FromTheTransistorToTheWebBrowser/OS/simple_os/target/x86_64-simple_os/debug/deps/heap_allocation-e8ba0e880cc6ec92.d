f9d9f2126a91505a5e88a8ff5a21efd58772a69e
FR4NC1SC011 <fcootzg@gmail.com>
2021-04-01 15:32:34 -0700

    Adding a Test

---
+++ FromTheTransistorToTheWebBrowser/OS/simple_os/target/x86_64-simple_os/debug/deps/heap_allocation-e8ba0e880cc6ec92.d
@@ -0,0 +1,5 @@
+/home/fco/Desktop/Projects/FromTheTransistorToTheWebBrowser/OS/simple_os/target/x86_64-simple_os/debug/deps/heap_allocation-e8ba0e880cc6ec92: tests/heap_allocation.rs
+
+/home/fco/Desktop/Projects/FromTheTransistorToTheWebBrowser/OS/simple_os/target/x86_64-simple_os/debug/deps/heap_allocation-e8ba0e880cc6ec92.d: tests/heap_allocation.rs
+
+tests/heap_allocation.rs:

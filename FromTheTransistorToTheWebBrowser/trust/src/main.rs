0bfa5b11bdc88d5d8f0b0c3d96a1e98bd4ce5346
FR4NC1SC011 <fcootzg@gmail.com>
2021-04-13 20:32:23 -0600

    Connection Structure Added

--- FromTheTransistorToTheWebBrowser/trust/src/main.rs
+++ FromTheTransistorToTheWebBrowser/trust/src/main.rs
@@ -14,7 +14,7 @@ struct Quad {
 
 fn main() -> io::Result<()> {
     let mut connections: HashMap<Quad, tcp::State> = Default::default();
-    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
@@ -36,14 +36,26 @@ fn main() -> io::Result<()> {
 
                 match etherparse::TcpHeaderSlice::from_slice(&buf[4 + iph.slice().len()..nbytes]) {
                     Ok(tcph) => {
+                        use std::collections::hash_map::Entry;
                         let datai = 4 + iph.slice().len() + tcph.slice().len();
-                        connections
-                            .entry(Quad {
-                                src: (src, tcph.source_port()),
-                                dst: (dst, tcph.destination_port()),
-                            })
-                            .or_default()
-                            .on_packet(iph, tcph, &buf[datai..nbytes]);
+                        match connections.entry(Quad {
+                            src: (src, tcph.source_port()),
+                            dst: (dst, tcph.destination_port()),
+                        }) {
+                            Entry::Occupied(c) => {
+                                c.on_packet(&mut nic, iph, tcph, &buf[datai..nbytes])?;
+                            }
+                            Entry::Vacant(e) => {
+                                if let Some(c) = tcp::Connection::accept(
+                                    &mut nic,
+                                    iph,
+                                    tcph,
+                                    &buf[datai..nbytes],
+                                )? {
+                                    e.insert(c);
+                                }
+                            }
+                        }
                     }
                     Err(e) => {
                         eprintln!("Ignoring weird tcp packet {:?}", e);
--- FromTheTransistorToTheWebBrowser/trust/src/tcp.rs
+++ FromTheTransistorToTheWebBrowser/trust/src/tcp.rs
@@ -1,51 +1,114 @@
+use std::io;
+
 pub enum State {
     Closed,
     Listen,
     SynRcvd,
-    Estab,
-
+    //Estab,
 }
 
-impl Default for State {
-    fn default() -> Self {
-    //    State {}
-        State::Listen
-    }
+pub struct Connection {
+    state: State,
+    send: SendSequenceSpace,
+    recv: RecvSequenceSpace,
 }
 
+struct SendSequenceSpace {
+    ///SND.UNA - send unacknowledged
+    una: u32,
+    ///SND.NXT - send next
+    nxt: u32,
+    ///SND.WND - send window
+    wnd: u16,
+    ///SND.UP  - send urgent pointer
+    up: bool,
+    ///SND.WL1 - segment sequence number used for last window update
+    wl1: usize,
+    ///SND.WL2 - segment acknowledgment number used for last window update
+    wl2: usize,
+    ///ISS     - initial send sequence number
+    iss: u32,
+}
 
-impl State {
-    pub fn on_packet<'a>(
-        &mut self,
+struct RecvSequenceSpace {
+    ///RCV.NXT - receive next
+    nxt: u32,
+    ///RCV.WND - receive window
+    wnd: u16,
+    ///RCV.UP  - receive urgent pointer
+    up: bool,
+    ///IRS     - initial receive sequence number
+    irs: u32,
+}
+
+impl Connection {
+    pub fn accept<'a>(
+        nic: &mut tun_tap::Iface,
         iph: etherparse::Ipv4HeaderSlice<'a>,
         tcph: etherparse::TcpHeaderSlice<'a>,
         data: &'a [u8],
-    ) {
+    ) -> io::Result<Option<Self>> {
+        let mut buf = [0u8; 1500];
 
-        match *self {
-            State::Closed => {
-                return;
-            }
-            State::Listen => {
-                if !tcph.syn() {
-                    // got unexpected SYN
-                    return;
-                }
-
-                let mut syn_ack = etherparse::TcpHeader::new(tcph.destination_port(), tcph.source_port(), 0, 0);
-                syn_ack.syn = true;
-                syn_ack.ack = true;
-            }
+        if !tcph.syn() {
+            // got unexpected SYN
+            return Ok(None);
         }
 
-        eprintln!(
-            "{} : {} -> {} : {} {}b of tcp",
-            iph.source_addr(),
-            tcph.source_port(),
-            iph.destination_addr(),
+        let iss = 0;
+        let mut c = Connection {
+            state: State::SynRcvd,
+            send: SendSequenceSpace {
+                iss,
+                una: iss,
+                nxt: iss + 1,
+                wnd: 10,
+                up: false,
+                wl1: 0,
+                wl2: 0,
+            },
+
+            recv: RecvSequenceSpace {
+                irs: tcph.sequence_number(),
+                nxt: tcph.sequence_number() + 1,
+                wnd: tcph.window_size(),
+                up: false,
+            },
+        };
+
+        let mut syn_ack = etherparse::TcpHeader::new(
             tcph.destination_port(),
-            data.len(),
+            tcph.source_port(),
+            c.send.iss,
+            c.send.wnd,
         );
-        }
+        syn_ack.acknowledgment_number = c.recv.nxt;
+        syn_ack.syn = true;
+        syn_ack.ack = true;
+        let mut ip = etherparse::Ipv4Header::new(
+            syn_ack.header_len(),
+            64,
+            etherparse::IpTrafficClass::Tcp,
+            [
+                iph.destination()[0],
+                iph.destination()[1],
+                iph.destination()[2],
+                iph.destination()[3],
+            ],
+            [
+                iph.source()[0],
+                iph.source()[1],
+                iph.source()[2],
+                iph.source()[3],
+            ],
+        );
+        let unwritten = {
+            let mut unwritten = &mut buf[..];
+            ip.write(&mut unwritten);
+            syn_ack.write(&mut unwritten);
+            unwritten.len();
+        };
+        nic.send(&buf[..unwritten])?;
+        Ok(Some(c))
     }
-
+}

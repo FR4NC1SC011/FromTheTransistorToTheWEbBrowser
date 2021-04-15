536dda2ed05f38f9a40ef50e50bf9a9df99519ea
FR4NC1SC011 <fcootzg@gmail.com>
2021-04-14 21:12:56 -0600

    Sequence Numbers Incomplete

--- FromTheTransistorToTheWebBrowser/trust/src/main.rs
+++ FromTheTransistorToTheWebBrowser/trust/src/main.rs
@@ -14,18 +14,18 @@ struct Quad {
 
 fn main() -> io::Result<()> {
     let mut connections: HashMap<Quad, tcp::State> = Default::default();
-    let mut nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut nic = tun_tap::Iface::without_packet_info("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
         //let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
-        if eth_proto != 0x0800 {
+        //let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        //if eth_proto != 0x0800 {
             // no ipv4
-            continue;
-        }
+        //    continue;
+        //}
 
-        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[..nbytes]) {
             Ok(iph) => {
                 let src = iph.source_addr();
                 let dst = iph.destination_addr();
@@ -34,16 +34,16 @@ fn main() -> io::Result<()> {
                     continue;
                 }
 
-                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + iph.slice().len()..nbytes]) {
+                match etherparse::TcpHeaderSlice::from_slice(&buf[iph.slice().len()..nbytes]) {
                     Ok(tcph) => {
                         use std::collections::hash_map::Entry;
-                        let datai = 4 + iph.slice().len() + tcph.slice().len();
+                        let datai = iph.slice().len() + tcph.slice().len();
                         match connections.entry(Quad {
                             src: (src, tcph.source_port()),
                             dst: (dst, tcph.destination_port()),
                         }) {
-                            Entry::Occupied(c) => {
-                                c.on_packet(&mut nic, iph, tcph, &buf[datai..nbytes])?;
+                            Entry::Occupied(mut c) => {
+                                c.get_mut().on_packet(&mut nic, iph, tcph, &buf[datai..nbytes])?;
                             }
                             Entry::Vacant(e) => {
                                 if let Some(c) = tcp::Connection::accept(
--- FromTheTransistorToTheWebBrowser/trust/src/tcp.rs
+++ FromTheTransistorToTheWebBrowser/trust/src/tcp.rs
@@ -1,16 +1,17 @@
 use std::io;
 
-pub enum State {
-    Closed,
-    Listen,
+enum State {
+    //Closed,
+    // Listen,
     SynRcvd,
-    //Estab,
+    Estab,
 }
 
 pub struct Connection {
     state: State,
     send: SendSequenceSpace,
     recv: RecvSequenceSpace,
+    ip: etherparse::Ipv4Header,
 }
 
 struct SendSequenceSpace {
@@ -42,6 +43,7 @@ struct RecvSequenceSpace {
 }
 
 impl Connection {
+    // fn send()
     pub fn accept<'a>(
         nic: &mut tun_tap::Iface,
         iph: etherparse::Ipv4HeaderSlice<'a>,
@@ -74,6 +76,23 @@ impl Connection {
                 wnd: tcph.window_size(),
                 up: false,
             },
+            ip: etherparse::Ipv4Header::new(
+                0,
+                64,
+                etherparse::IpTrafficClass::Tcp,
+                [
+                    iph.destination()[0],
+                    iph.destination()[1],
+                    iph.destination()[2],
+                    iph.destination()[3],
+                ],
+                [
+                    iph.source()[0],
+                    iph.source()[1],
+                    iph.source()[2],
+                    iph.source()[3],
+                ],
+            ),
         };
 
         let mut syn_ack = etherparse::TcpHeader::new(
@@ -85,30 +104,81 @@ impl Connection {
         syn_ack.acknowledgment_number = c.recv.nxt;
         syn_ack.syn = true;
         syn_ack.ack = true;
-        let mut ip = etherparse::Ipv4Header::new(
-            syn_ack.header_len(),
-            64,
-            etherparse::IpTrafficClass::Tcp,
-            [
-                iph.destination()[0],
-                iph.destination()[1],
-                iph.destination()[2],
-                iph.destination()[3],
-            ],
-            [
-                iph.source()[0],
-                iph.source()[1],
-                iph.source()[2],
-                iph.source()[3],
-            ],
-        );
+        c.ip.set_payload_len(syn_ack.header_len() as usize + 0);
+
+        //syn_ack.checksum = syn_ack.calc_checksum_ipv4(&ip, &[]).expect("failed to compute checksum");
+        eprintln!("got ip header: \n {:02x?}", iph);
+        eprintln!("got tcp header: \n {:02x?}", tcph);
+
         let unwritten = {
             let mut unwritten = &mut buf[..];
-            ip.write(&mut unwritten);
+            c.ip.write(&mut unwritten);
             syn_ack.write(&mut unwritten);
             unwritten.len();
         };
         nic.send(&buf[..unwritten])?;
         Ok(Some(c))
     }
+
+    pub fn on_packet<'a>(
+        &mut self,
+        nic: &mut tun_tap::Iface,
+        iph: etherparse::Ipv4HeaderSlice<'a>,
+        tcph: etherparse::TcpHeaderSlice<'a>,
+    ) -> io::Result<()> {
+        // first check that sequence numbers are valid (RFC 793 S3.3)
+        //
+        // acceptable ack check
+        // SND.UNA < SEG.ACK < SND.NXT
+        // but remember wrapping!!!
+        //
+        let ackn = tcph.acknowledgment_number();
+        if !is_between_wrapped(self.send.una, ackn, self.send.nxt.wrapping_add(1)) {
+            return Ok(());
+        }
+
+        // valid segment check
+        let seqn = tcph.sequence_number();
+        let wend = self.recv.nxt.wrapping_add(self.recv.wnd as u32);
+        if !is_between_wrapped(self.recv.nxt.wrapping_sub(1), seqn, wend)
+            && !is_between_wrapped(
+                self.recv.nxt.wrapping_sub(1),
+                seqn + data.len() as u32 - 1,
+                wend,
+            )
+        {
+            return Ok(());
+        }
+
+        match self.state {
+            State::SynRcvd => {
+                // expect to get an ACK for our SYN
+            }
+
+            State::Estab => {
+                unimplemented!();
+            }
+        }
+
+        Ok(())
+    }
+}
+fn is_between_wrapped(start: u32, x: u32, end: u32) -> bool {
+    use std::cmp::Ordering;
+    match start.cmp(&x) {
+        Ordering::Equal => return false,
+        Ordering::Less => {
+            if end >= start && end <= x {
+                return false;
+            }
+        }
+        Ordering::Greater => {
+            // check is ok if only if n is between u and a
+            if end < start && end > x {
+            } else {
+                return false;
+            }
+        }
+    }
+    true
 }

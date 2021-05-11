b7a7cfaff2716c8dc85a22ff3eb7869c744adae7
FR4NC1SC011 <fcootzg@gmail.com>
2021-05-11 12:48:17 -0600

    Finishing things up

--- FromTheTransistorToTheWebBrowser/browser_engine/src/lib.rs
+++ FromTheTransistorToTheWebBrowser/browser_engine/src/lib.rs
@@ -1,6 +1,15 @@
+#[macro_use]
+extern crate gfx;
+extern crate gfx_text;
+extern crate gfx_window_glutin;
+extern crate glutin;
+
+
+pub mod render;
 pub mod dom;
 pub mod html_parse;
 pub mod css;
 pub mod css_parser;
 pub mod style;
 pub mod layout;
+pub mod command;

9926ed0996e7f7d2c0bb8675f10fb93342c87c32
FR4NC1SC011 <fcootzg@gmail.com>
2021-06-09 20:56:58 -0600

    Fixed fetch_word function and added LDA Absolute

--- FromTheTransistorToTheWebBrowser/rust-6502/src/mos6502.rs
+++ FromTheTransistorToTheWebBrowser/rust-6502/src/mos6502.rs
@@ -1,46 +1,44 @@
 use num_traits::WrappingShl;
-use std::num::Wrapping;
 use std::os::raw::*;
 
-//TODO: alias declaration?
-// Byte = c_uchar
-// Word = c_ushort
+type Byte = c_uchar;
+type Word = c_ushort;
 
 #[derive(Debug)]
 pub struct Mem {
     pub MAX_MEM: u32,
-    pub Data: Vec<c_uchar>,
+    pub Data: Vec<Byte>,
 }
 
 #[derive(Debug)]
 pub struct CPU {
-    pub PC: c_ushort, // program counter
-    pub SP: c_ushort, // stack pointer
+    pub PC: Word, // program counter
+    pub SP: Word, // stack pointer
 
     // Registers
-    pub A: c_uchar, // Accumulator
-    pub X: c_uchar,
-    pub Y: c_uchar,
+    pub A: Byte, // Accumulator
+    pub X: Byte,
+    pub Y: Byte,
 
     // Status flags
-    pub C: c_uchar,
-    pub Z: c_uchar,
-    pub I: c_uchar,
-    pub D: c_uchar,
-    pub B: c_uchar,
-    pub V: c_uchar,
-    pub N: c_uchar,
+    pub C: Byte,
+    pub Z: Byte,
+    pub I: Byte,
+    pub D: Byte,
+    pub B: Byte,
+    pub V: Byte,
+    pub N: Byte,
 
     // Opcodes
-    pub INS_LDA_IM: c_uchar,
-    pub INS_LDA_ZP: c_uchar,
-    pub INS_LDA_ZPX: c_uchar,
-    pub INS_LDA_ABS: c_uchar,
-    pub INS_LDA_ABSX: c_uchar,
-    pub INS_LDA_ABSY: c_uchar,
-    pub INS_LDA_INDX: c_uchar,
-    pub INS_LDA_INDY: c_uchar,
-    pub INS_JSR: c_uchar, // TODO: Fix overflow
+    pub INS_LDA_IM: Byte,
+    pub INS_LDA_ZP: Byte,
+    pub INS_LDA_ZPX: Byte,
+    pub INS_LDA_ABS: Byte,
+    pub INS_LDA_ABSX: Byte,
+    pub INS_LDA_ABSY: Byte,
+    pub INS_LDA_INDX: Byte,
+    pub INS_LDA_INDY: Byte,
+    pub INS_JSR: Byte, // TODO: Fix overflow
 }
 
 impl Mem {
@@ -58,7 +56,7 @@ impl Mem {
     }
 
     // write 2 bytes
-    fn write_word(&mut self, value: c_ushort, address: u32, cycles: &mut usize) {
+    fn write_word(&mut self, value: Word, address: u32, cycles: &mut usize) {
         self.Data[address as usize] = (value & 0xFF) as u8;
         self.Data[(address + 1) as usize] = (value >> 8) as u8;
         *cycles -= 2;
@@ -115,14 +113,14 @@ impl CPU {
         memory.initialize();
     }
 
-    fn fetch_word(&mut self, cycles: &mut usize, memory: &mut Mem) -> c_ushort {
+    fn fetch_word(&mut self, cycles: &mut usize, memory: &mut Mem) -> Word {
         // 6502 is little endian
-        let mut data: c_ushort = memory.Data[self.PC as usize] as u16;
+        let mut data: Word = memory.Data[self.PC as usize] as Word;
+        println!("data: {}", data);
         self.PC += 1;
 
-        // data |= (memory.Data[self.PC as usize] << 8 as u32) as u16;
-        // data = data.wrapping_shl(memory.Data[self.PC as usize] as u32);
-        data = data + WrappingShl::wrapping_shl(&data, 8);
+        data |= WrappingShl::wrapping_shl(&(memory.Data[self.PC as usize] as Word), 8);
+        println!("data: {}", data);
 
         self.PC += 1;
 
@@ -130,15 +128,15 @@ impl CPU {
         data
     }
 
-    fn fetch_byte(&mut self, cycles: &mut usize, memory: &mut Mem) -> c_uchar {
-        let data: c_uchar = memory.Data[self.PC as usize];
+    fn fetch_byte(&mut self, cycles: &mut usize, memory: &mut Mem) -> Byte {
+        let data: Byte = memory.Data[self.PC as usize];
         self.PC += 1;
         *cycles -= 1;
         data
     }
 
-    fn read_byte(&mut self, cycles: &mut usize, address: c_uchar, memory: &mut Mem) -> c_uchar {
-        let data: c_uchar = memory.Data[address as usize];
+    fn read_byte(&mut self, cycles: &mut usize, address: Word, memory: &mut Mem) -> Byte {
+        let data: Byte = memory.Data[address as usize];
         *cycles -= 1;
         data
     }
@@ -146,40 +144,48 @@ impl CPU {
     pub fn execute(&mut self, cycles: &mut usize, memory: &mut Mem) -> usize {
         let cycles_requested = *cycles;
         while cycles > &mut 0 {
-            let ins: c_uchar = self.fetch_byte(cycles, memory);
+            let ins: Byte = self.fetch_byte(cycles, memory);
 
             match ins {
                 0xA9 => {
-                    println!("Instruction Load A");
-                    let value: c_uchar = self.fetch_byte(cycles, memory);
+                    println!("Instruction LDA Inmediate");
+                    let value: Byte = self.fetch_byte(cycles, memory);
                     self.A = value;
                     self.lda_set_status();
                 }
 
                 0xA5 => {
                     println!("Instruction Load ZP");
-                    let zero_page_address: c_uchar = self.fetch_byte(cycles, memory);
-                    self.A = self.read_byte(cycles, zero_page_address, memory);
+                    let zero_page_address: Byte = self.fetch_byte(cycles, memory);
+                    self.A = self.read_byte(cycles, zero_page_address as u16, memory);
                     self.lda_set_status();
                 }
 
                 0xB5 => {
                     println!("Instruction Load ZPX");
-                    let mut zero_page_address: c_uchar = self.fetch_byte(cycles, memory);
+                    let mut zero_page_address: Byte = self.fetch_byte(cycles, memory);
                     zero_page_address = zero_page_address.wrapping_add(self.X);
                     *cycles -= 1;
-                    self.A = self.read_byte(cycles, zero_page_address, memory);
+                    self.A = self.read_byte(cycles, zero_page_address as u16, memory);
                     self.lda_set_status();
                 }
 
                 0x20 => {
                     println!("Instruction Load JSR");
-                    let mut sub_addr: c_ushort = self.fetch_word(cycles, memory);
+                    let mut sub_addr: Word = self.fetch_word(cycles, memory);
                     memory.write_word(self.PC - 1, self.SP as u32, cycles);
                     self.SP += 2;
                     self.PC = sub_addr;
                     *cycles -= 1;
                 }
+
+                0xAD => {
+                    println!("Instruction LDA Absolute");
+                    let abs_addrress: Word = self.fetch_word(cycles, memory);
+                    println!("address: {}", abs_addrress);
+                    self.A = self.read_byte(cycles, abs_addrress as u16, memory);
+                    println!("A: {}", self.A);
+                } 
                 _ => eprintln!("Instruction not handled {}", ins),
             }
         }

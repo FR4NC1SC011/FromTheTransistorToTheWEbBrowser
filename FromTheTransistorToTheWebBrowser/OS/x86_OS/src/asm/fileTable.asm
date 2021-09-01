e428964da901fbd9c6a87c1a7456e2a2405664bf
FR4NC1SC011 <fcootzg@gmail.com>
2021-08-31 20:28:21 -0600

    Warm Reboot and Print File Table

--- FromTheTransistorToTheWebBrowser/OS/x86_OS/src/asm/fileTable.asm
+++ FromTheTransistorToTheWebBrowser/OS/x86_OS/src/asm/fileTable.asm
@@ -2,7 +2,7 @@
 ;;; Basic 'file table' made with db. Strings consist of '{fileName1-sector#, fileName2-sector#, ... fileNameN-sector#}'
 ;;; 
 
-db '{calculator-04, program2-06}'
+db '{calculator-04,program2-06}'
 
   ;; sector padding magic!
   times 512-($-$$) db 0         ;; pad rest of file to 0s till 512th byte/end of sector
--- FromTheTransistorToTheWebBrowser/OS/x86_OS/src/asm/kernel.asm
+++ FromTheTransistorToTheWebBrowser/OS/x86_OS/src/asm/kernel.asm
@@ -36,24 +36,88 @@ keyloop:
 run_command:
   mov byte [di], 0              ; null terminate cmdString from di
   mov al, [cmdString]
-  cmp al, 'F'
-  jne not_found
-  cmp al, 'N'
+  cmp al, 'F'                   ; File table command
+  je filebrowser
+  cmp al, 'R'                   ; Reboot
+  je reboot
+  cmp al, 'N'                   ; end our current program
   je end_program
-  mov si, success
+  mov si, failure               ; command not found, boo!
   call print_string
   jmp get_input
   
   
-not_found:
-  mov si, failure
+filebrowser:                    ; Menu F)  - File Browser
+  ;; Reset Screen State
+  ;; ------------------
+  ;; Set Video Mode
+  mov ah, 0x00                  ; int 0x10/ ah 0x00 = set video mode
+  mov al, 0x03                  ; 80x25 text mode
+  int 0x10
+
+  ;; Change color/Palette
+  mov ah, 0x0B
+  mov bh, 0x00
+  mov bl, 0x01
+  int 0x10
+
+  mov si, fileTableHeading
   call print_string
-  jmp get_input
+
+  ;; Load File Table string from its memory location (0x1000:0000)
+  xor cx, cx                    ; reset counter for # chars in file
+  mov ax, 0x1000                ; file table location
+  mov es, ax                    ; ES = 0x1000
+  xor bx, bx                    ; ES:BX = 0x1000:0
+  mov ah, 0x0e                  ; get ready to print to screen
+
+fileTable_loop:
+  inc bx
+  mov al, [ES:BX]
+  cmp al, '}'                   ; end of filetable?
+  je stop
+  cmp al, '-'                   ; at sector number of element?
+  je sectorNumber_loop
+  cmp al, ','                   ; between table elements?
+  je next_element
+  inc cx
+  int 0x10
+  jmp fileTable_loop
+
+sectorNumber_loop:
+  cmp cx, 21
+  je fileTable_loop
+  mov al, ' '
+  int 0x10
+  inc cx
+  jmp sectorNumber_loop
+
+next_element:
+  xor cx, cx
+  mov al, 0xA
+  int 0x10
+  mov al, 0xD
+  int 0x10
+  mov al, 0xA
+  int 0x10
+  mov al, 0xD
+  int 0x10
+  jmp fileTable_loop
+
+stop:
+  hlt
+  
+  
+
+reboot:                         ; Far jump to Reset Vector
+  jmp 0xFFFF:0x0000
 
 end_program:
   ;; End Pgm
   cli
   hlt
+
+
 
 print_string:
   mov ah, 0x0e                  ; int 10h/ ah 0x0e BIOS teletype output
@@ -74,10 +138,16 @@ end_print:
 menuString: db '--------------------------------', 0xA, 0xD,\ 
                'Kernel Booted, Welcome to my OS', 0xA, 0xD,\
                '--------------------------------', 0xA, 0xD, 0xA, 0xD,\
-               'F) File & Program Browser', 0xA, 0xD, 0
+               'F) File & Program Browser', 0xA, 0xD,\
+               'R) Reboot', 0xA, 0xD, 0
 
 success: db 0xA, 0xD, 'Command ran successfully', 0xA, 0xD, 0
 failure: db 0xA, 0xD, 'Command not found :(', 0xA, 0xD, 0
+
+fileTableHeading: db '--------------          ----------',\
+                      'File/Program           Sector', 0xA, 0xD,\
+                      '-------------          ----------', 0xA, 0xD, 0
+
 cmdString: db '', 0
 
   ;; Sector padding magic
